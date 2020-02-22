2ccd54939f0ac72c2dffb5ff24b7788ba431e349
Juannino27 <60374311+Juannino27@users.noreply.github.com>
2020-02-21 22:37:55 -0800

    listo

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -1,63 +1,38 @@
 ﻿using System;
 
 namespace Peliculas
-{class Pelicula
+{class Pelicula 
+{
+    private string Titulo;
+    private int Año;
+    private string Pais;
+    private string Director;
+
+    public Pelicula(string Titulo, int Año, string Pais, string Director)
     {
-        private string Titulo, Director, Pais;
-        private int año;
-        
-        public string getTitulo()
-        {
-            return Titulo;
-        }
-        public void setTitulo(string t)
-        {
-            Titulo = t;
-        }
-        public string getDirrector()
-        {
-            return Director;
-        }
-        public void setDirector(string d)
-        {
-            Director =d;
-        }
-        public string getPais()
-        {
-            return Pais;
-        }
-        public void setPais(string p)
-        {
-            Pais = p;
-        }
-        public int getAño()
-        {
-            return año;
-        }
-        public void setAño(int a)
-        {
-            año = a;
-        }
+        this.Titulo = Titulo;
+        this.Año = Año;
+        this.Pais = Pais;
+        this.Director = Director;
     }
+    // Se crea el Metodo imprime
+    public void imprime()
+    {
+        Console.WriteLine("{0}({1}, {2}, {3})", this.Titulo, this.Año, this.Pais, this.Director);
+    }
+}
+
     class Program
     {
         static void Main(string[] args)
-        {   //Declaración de los objetos de la clase Pelicula
-            Pelicula Pel1 = new Pelicula();
-            Pelicula Pel2 = new Pelicula();
-            //Metodo Set y Get para el primer constructor
-            Pel1.setTitulo("Titanic");
-            Pel1.setAño(1997);
-            Pel1.setPais("Estados Unidos");
-            Pel1.setDirector("James Cameron");
-            Console.WriteLine("{0}({1},{2},{3})", Pel1.getTitulo(), Pel1.getAño(), Pel1.getPais(), Pel1.getDirrector());
-            //Metodo Set y Get para el segundo constructor
-            Pel2.setTitulo("Black Panther");
-            Pel2.setAño(2018);
-            Pel2.setPais("Estados Unidos");
-            Pel2.setDirector("Ryan Coogler");
-            Console.WriteLine("{0}({1},{2},{3})", Pel2.getTitulo(), Pel2.getAño(), Pel2.getPais(), Pel2.getDirrector());
+        {//Objetos clase Pelicula
+            Pelicula Peli1 = new Pelicula("Titanic", 1994, "Estados Unidos", "James Cameron");
+            Pelicula Peli2 = new Pelicula("Black Panther", 2018, "Estados Unidos", "Ryan Coogler");
+            //  Se llama al metodo imprime
+            Peli1.imprime();
+            Peli2.imprime();
         }
     }
 }
+
 
