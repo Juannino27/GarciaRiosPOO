36a54528beae71d8336fb8b8a3668691291cbe3a
Juannino27 <60374311+Juannino27@users.noreply.github.com>
2020-03-10 16:59:38 -0700

    Variable tipo valor

---
+++ Pase_Parametros/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Pase_Parametros
+{//Creacion clase Numero
+   class Numero
+   {//Variable tipo valor
+       public void Potencia(int p)
+       {
+           p = p * p * p;
+       }
+       
+   }
+   //Creacion clase Concursante
+   class Concursante
+   {//Variable tipo valor
+       public string nombre;
+
+       public Concursante()
+       {
+           nombre = "Juan";
+       }
+   }
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            //Objeto nuevo de clase Numero y Concursante
+            Numero nuevo = new Numero();
+            Concursante F = new Concursante();
+            //Pase de parametros tipo valor
+            nuevo.Potencia(a);
+            Console.WriteLine(a);
+            Console.WriteLine("{0} {1}", "Sigue Intentando:", F.nombre);
+
+        }
+    }
+}
