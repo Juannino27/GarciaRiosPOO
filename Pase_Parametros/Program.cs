9b44763fda03885ac750fc7fce0bfb3099954d59
Juannino27 <60374311+Juannino27@users.noreply.github.com>
2020-03-10 17:16:46 -0700

    Variable referencia ref

--- Pase_Parametros/Program.cs
+++ Pase_Parametros/Program.cs
@@ -8,6 +8,11 @@ namespace Pase_Parametros
        {
            p = p * p * p;
        }
+       //Variable tipo ref
+       public void PotenciaRef(ref int p)
+       {
+           p = p * p * p;
+       }
        
    }
    //Creacion clase Concursante
@@ -20,7 +25,22 @@ namespace Pase_Parametros
            nombre = "Juan";
        }
    }
-   
+   //Clase Cambio, variable tipo referencia
+   class Cambio
+   {
+       static public void Cambiar(Concursante pro)
+       {
+           pro.nombre = "Mike";
+       }
+   }
+   //Clase Concurso, variable tipo ref
+   class Concurso
+   {
+       static public void Suerte(ref Concursante J)
+       {
+           J.nombre ="Luis";
+       }
+   }
     class Program
     {
         static void Main(string[] args)
@@ -32,8 +52,18 @@ namespace Pase_Parametros
             //Pase de parametros tipo valor
             nuevo.Potencia(a);
             Console.WriteLine(a);
-            Console.WriteLine("{0} {1}", "Sigue Intentando:", F.nombre);
+            //Pase de parametros tipo ref
+            nuevo.PotenciaRef(ref a);
+            Console.WriteLine(a);
 
+            
+            //Pase de parametros tipo valor
+            Console.WriteLine("{0} {1}", "Sigue Intentando: ", F.nombre);
+            //Pase de parametros tipo referencia ref
+            Cambio.Cambiar(F); 
+            Console.WriteLine("{0} {1}", "Sigue intentando: ", F.nombre);
+            Concurso.Suerte(ref F);
+            Console.WriteLine("{0} {1}", "Felicidades campeón: ", F.nombre);
         }
     }
 }
